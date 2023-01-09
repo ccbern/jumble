1f054dac24e6b35267c91cfdb81c46d03a6f3425
Chase Bernhardt <42153097+ccbern@users.noreply.github.com>
2023-01-09 09:51:08 -0700

    Add files via upload
    Updated

--- jumble_with_func.py
+++ jumble_with_func.py
@@ -1,45 +1,44 @@
 import requests
 import random
 
-word_list = []
-pared_word_list = []
-new_word = True
-random_word = ''
-random_order = ''
-
 ### GET LIST OF WORDS ###
 def get_word_list(word_site):
+	word_list = []
 	response = requests.get(word_site)
 	words = response.content.splitlines()
-
-	# word_list = []
 
 	for w in words:
 		word_list.append(w.decode('ASCII'))
 
+	return word_list
+
 ### PARE WORD LIST BY LENGTH ###
 def pare_words_by_length(min_length,max_length):
-	# pared_word_list = []
+	pared_word_list = []
 
 	for word in word_list:
 		if len(word) >= min_length and len(word) <= max_length:
 			pared_word_list.append(word)
 
+	return pared_word_list
+
 ### PICK NEW WORD ###
-def pick_new_word():
-	# while new_word:
-	guess = 'first'
-	random_order = []
+def pick_new_word(pared_word_list):
 	random_word_num = random.randrange(len(pared_word_list))
 	random_word = pared_word_list[random_word_num]
 
 	### REMOVE PICKED WORD FROM WORD LIST ###
 	pared_word_list.remove(random_word)
 
+	return random_word
+
 ### JUMBLE AND PRINT WORD ###
 def jumble_word(word):
+	random_order = []  ### CLEARED TWICE -- BOO ###
+	random_order_string = ''
 	word_to_break = word
-	
+
+	### vv Why adding to existing string? vv ###
 	for letter in word_to_break:
 		random_let_num = random.randrange(len(word_to_break))
 		random_letter = word_to_break[random_let_num]
@@ -48,48 +47,85 @@ def jumble_word(word):
 
 	random_order_string = ''.join(random_order)
 
-	print(random_order_string)
+	return random_order_string
+	### ^^ Why adding to existing string? ^^ ###
 
 ### GUESSING ###
-def try_guess(guess):
+def try_guess(guess,random_word):
 	correct_guess = False
-	if guess == random_word:
+	if guess.lower() == random_word.lower():
 		correct_guess = True
 		return True
 
 	else:
 		return False
 
+### RUN GAME ###
 print("Let's play JUMBLE!")
 
-get_word_list("https://www.mit.edu/~ecprice/wordlist.10000")
+word_list = get_word_list("https://www.mit.edu/~ecprice/wordlist.10000")
 
 ### GET LENGTH FROM USER ###
 min_length = int(input('Minimum word length: '))
-max_length = int(input('Max word length: '))
+max_length = int(input('Maximum word length: '))
 
-pare_words_by_length(min_length,max_length)
+pared_word_list = pare_words_by_length(min_length,max_length)
 
-### RUN GAME ###
+correct_guesses = 0
+current_streak = 0
+highest_streak = 0
+new_word = True
+
 while new_word:
-	pick_new_word()
-	jumble_word(random_word)
+	guess_count = 0
+	random_word = pick_new_word(pared_word_list)
+	jumbled_word = jumble_word(random_word)
+	print('')
+	print(jumbled_word)
+	print('')
+
 	correct = False
 
 	while not correct:
-		guess = input('Guess the word!  For answer, type "Give up": ')
+		print('Guess the word!')  
+		print('Type "Scramble" to re-jumble the letters or "Give up" for the answer.')
+		guess = input('Guess: ')
 
 		if guess.capitalize() == 'Give up':
 			print('Too bad!  The word was %s' % random_word.upper())
-	
-		else:
-			if try_guess(guess):
-				print('You got it!  The word was %s' % random_word.upper())
+			correct = True
 
+			if current_streak != 0:
+				print('Streak lost!  Previous streak was: %i' % current_streak)
+			
+			current_streak = 0
+
+		elif guess.capitalize() == 'Scramble':
+			rescrambled_word = jumble_word(jumbled_word)
+			print(rescrambled_word)
+			
+		else:
+			guess_count += 1
+
+			if try_guess(guess,random_word):
+				print('You got it!  The word was %s' % random_word.upper())
+				correct_guesses += 1
 				correct = True
-	
+
+				if guess_count == 1:
+					current_streak += 1
+
+					if current_streak > highest_streak:
+						highest_streak = current_streak
+						print('New high streak!')
+
+				print('Current streak is: %i' % current_streak)
+
 			else:
+				if current_streak != 0:
+					print('Streak lost!  Previous streak was: %i' % current_streak)
 				print('Try again!')
+				current_streak = 0
 
 	replay = input('Another word?  "Y" or "N": ').upper()
 	
@@ -97,5 +133,7 @@ while new_word:
 		replay = (input('Please choose "Y" or "N": ').upper())
 
 	if replay == 'N':
+		print('You got %i words correct this time.' % correct_guesses)
+		print('Your highest streak was: %i' % highest_streak)
 		print('Thanks for playing!')
 		new_word = False
